112a63b4c7ea84db6e63dcca3903619d43591f75
LunareMagicaFata <superlupetto@virgilio.it>
2026-02-17 12:32:16 +0100

    Update freeSuperDownloader.py

--- freeSuperDownloader.py
+++ freeSuperDownloader.py
@@ -12,7 +12,7 @@ MUSIC_DIR = os.path.join(BASE_DIR, "Musica")
 VIDEO_DIR = os.path.join(BASE_DIR, "Video")
 FFMPEG_ROOT = r"C:\FFmpeg"
 FFMPEG_EXE = os.path.join(FFMPEG_ROOT, "bin", "ffmpeg.exe")
-UPDATE_URL = "http://lunaremagicafata.duckdns.org/downloads/FreeSuperDownloader.py"
+UPDATE_URL = "http://lunaremagica.duckdns.org/downloads/FreeSuperDownloader.py"
 FF_URL = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip"
 SCRIPT_PATH = os.path.abspath(__file__)
 
@@ -120,3 +120,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
